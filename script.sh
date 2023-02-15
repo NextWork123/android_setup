67981e74068ce13adb63e3839ddb9bb54fa0bd7d
Peppe289 <gsperanza204@gmail.com>
2023-02-15 11:49:33 +0100

    split operation in func
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- script.sh
+++ script.sh
@@ -7,6 +7,12 @@ GRN='\033[0;32m'
 PUL='\033[0;35m'
 RST='\033[0m'
 
+# Current Linux Distribution
+distro=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr '[:upper:]' '[:lower:]' | sed 's/\"//g')
+
+MEMORY_RAM=0
+MEMORY_SWAP=0
+
 loadMemory() {
     MEMORY_RAM=$(cat /proc/meminfo | grep MemTotal: | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g')
     # Convert to MB
@@ -20,15 +26,6 @@ loadMemory() {
     # Convert to GB
     MEMORY_SWAP=$((${MEMORY_SWAP} / 1024))
 }
-
-#
-# Get RAM size.
-# If RAM < 16 GB optimize swap usage.
-# Check also for already swap partition.
-#
-MEMORY_RAM=0
-MEMORY_SWAP=0
-loadMemory
 
 SwapOnPartition() {
     #
@@ -155,159 +152,199 @@ SwapOnFile() {
     printf "${BLU}New swap size: $MEMORY_SWAP GB${RST}\n"
 }
 
-# Current Linux Distribution
-distro=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr '[:upper:]' '[:lower:]' | sed 's/\"//g')
-
-# Ask for user name and email for Github
-if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
-    echo "Enter your Github name and email to configure Git:"
-    read -p "Name: " YOUR_NAME
-    read -p "Email: " YOUR_EMAIL
-    git config --global user.name "$YOUR_NAME"
-    git config --global user.email "$YOUR_EMAIL"
-else
-    echo "Git is already configured with the following details:"
-    echo "Name: $(git config --global user.name)"
-    echo "Email: $(git config --global user.email)"
-    read -p "Do you want to change these details? [y/N]: " change_details
-    if [ "$change_details" == "y" ] || [ "$change_details" == "Y" ]; then
-        read -p "Enter new name: " YOUR_NAME
-        read -p "Enter new email: " YOUR_EMAIL
+set_git_user() {
+    # Ask for user name and email for Github
+    if [ -z "$(git config --global user.name)" ] || [ -z "$(git config --global user.email)" ]; then
+        echo "Enter your Github name and email to configure Git:"
+        read -p "Name: " YOUR_NAME
+        read -p "Email: " YOUR_EMAIL
         git config --global user.name "$YOUR_NAME"
         git config --global user.email "$YOUR_EMAIL"
+    else
+        echo "Git is already configured with the following details:"
+        echo "Name: $(git config --global user.name)"
+        echo "Email: $(git config --global user.email)"
+        read -p "Do you want to change these details? [y/N]: " change_details
+        if [ "$change_details" == "y" ] || [ "$change_details" == "Y" ]; then
+            read -p "Enter new name: " YOUR_NAME
+            read -p "Enter new email: " YOUR_EMAIL
+            git config --global user.name "$YOUR_NAME"
+            git config --global user.email "$YOUR_EMAIL"
+        fi
     fi
-fi
+}
 
-# Cloning of akhilnarang Scripts in '/home/$USER/scripts' and execution
-SCRIPT_DIR=~/scripts
-if [[ ! -f ~/scripts/setup/android_build_env.sh ]]; then
-    git clone https://github.com/akhilnarang/scripts --depth=1 $SCRIPT_DIR
-    #
-    # Abort the script if it failed to clone.
-    # Problems with github? Problems with the user's internet?
-    # What do I know?
-    #
-    if [ $? -ne 0 ]; then
-        echo "Failled to clone akhilnarang/scripts in $SCRIPT_DIR"
+script_install() {
+    # Cloning of akhilnarang Scripts in '/home/$USER/scripts' and execution
+    SCRIPT_DIR=~/scripts
+    if [[ ! -f ~/scripts/setup/android_build_env.sh ]]; then
+        git clone https://github.com/akhilnarang/scripts --depth=1 $SCRIPT_DIR
+        #
+        # Abort the script if it failed to clone.
+        # Problems with github? Problems with the user's internet?
+        # What do I know?
+        #
+        if [ $? -ne 0 ]; then
+            echo "Failled to clone akhilnarang/scripts in $SCRIPT_DIR"
+            exit 1
+        fi
+    fi
+    cd $SCRIPT_DIR
+
+    # Installation of necessary packages
+    case "$distro" in
+    "ubuntu" | "debian" | "linuxmint" | "kali")
+        ./setup/android_build_env.sh
+        ;;
+    "arch" | "manjaro" | "arcolinux" | "garuda" | "artix" | "cachyos")
+        ./setup/arch-manjaro.sh
+        ;;
+    "solus")
+        ./setup/solus.sh
+        ;;
+    "fedora" | "centos")
+        ./setup/fedora.sh
+        ;;
+    *)
+        echo "Distribution Not Supported"
         exit 1
-    fi
-fi
-cd $SCRIPT_DIR
+        ;;
+    esac
+}
 
-# Installation of necessary packages
-case "$distro" in
-"ubuntu" | "debian" | "linuxmint" | "kali")
-    ./setup/android_build_env.sh
-    ;;
-"arch" | "manjaro" | "arcolinux" | "garuda" | "artix" | "cachyos")
-    ./setup/arch-manjaro.sh
-    ;;
-"solus")
-    ./setup/solus.sh
-    ;;
-"fedora" | "centos")
-    ./setup/fedora.sh
-    ;;
-*)
-    echo "Distribution Not Supported"
-    exit 1
-    ;;
-esac
+ccache_settings() {
+    # Check if ccache config is already present in .bashrc
+    if ! grep -q 'Generated ccache config' "$HOME/.bashrc"; then
+        echo "Configuring ccache..."
 
-# Check if ccache config is already present in .bashrc
-if ! grep -q 'Generated ccache config' "$HOME/.bashrc"; then
-    echo "Configuring ccache..."
+        # Set the ccache directory
+        export CCACHE_DIR=~/.ccache
+        # Set the path to ccache executable
+        export CCACHE_EXEC=$(which ccache)
+        # Enable ccache
+        export USE_CCACHE=1
+        # Set cache size to 50 GB
+        ccache -M 50G >/dev/null
+        # Enable compression
+        ccache -o compression=true >/dev/null
+        # Zero statistics
+        ccache -z >/dev/null
 
-    # Set the ccache directory
-    export CCACHE_DIR=~/.ccache
-    # Set the path to ccache executable
-    export CCACHE_EXEC=$(which ccache)
-    # Enable ccache
-    export USE_CCACHE=1
-    # Set cache size to 50 GB
-    ccache -M 50G >/dev/null
-    # Enable compression
-    ccache -o compression=true >/dev/null
-    # Zero statistics
-    ccache -z >/dev/null
-
-    # Append ccache config to .bashrc
-    cat <<EOF >>"$HOME/.bashrc"
+        # Append ccache config to .bashrc
+        cat <<EOF >>"$HOME/.bashrc"
 
 # Generated ccache config
 export USE_CCACHE=1
 export CCACHE_EXEC=$CCACHE_EXEC
 export CCACHE_DIR=$CCACHE_DIR
 EOF
-fi
+    fi
+
+}
 
 #
 # True only if RAM is less than 16 GB and SWAP is less than 30 GB
 #
-if [ $MEMORY_RAM -lt 16 ] && [ $MEMORY_SWAP -lt 30 ]; then
-    echo "Your ram is less than 16 GB ($MEMORY_RAM GB) and your current swap is $MEMORY_SWAP GB"
-    read -p "Do you want to add a swap partition? [y/N]: " enable_swap
-    if [ "$enable_swap" == "y" ] || [ "$enable_swap" == "Y" ]; then
-        echo "1) Swap on disk partition"
-        echo "2) Swap on file"
-        echo "0 (or other) exit"
-        read -p "Chose [1-2]: " enable_swap
-        if [[ "$enable_swap" == "1" ]]; then
-            printf "${RED}WARNING: This script will FORMAT all the partition selected as SWAP."
-            printf "\nIf you are not sure what do selection default N${RST}\n"
-            read -p "Do you want continue? [y/N]" ImSure
-            if [ "$ImSure" == "y" ] || [ "$ImSure" == "Y" ]; then
-                SwapOnPartition
-            else
-                echo "Abort."
-            fi
-        elif [[ "$enable_swap" == "2" ]]; then
-            SwapOnFile
-        fi
-    fi
-fi
-
-#
-# Increase swap usage if your machine is less than 16
-#
-if [[ $MEMORY_RAM -lt 16 ]]; then
-    if free | awk '/^Swap:/ {exit !$2}'; then
-        SWAPPINESS="/proc/sys/vm/swappiness"
-        if [ -f $SWAPPINESS ]; then
-            #
-            # 200 is max for linux 5.10 or over.
-            # 100 is max for linux 5.4 or before.
-            #
-            VALUE="100" # 100 by default
-            #
-            # put VERSION - PATCHLEVEL - SUBLEVEL in array
-            # but we need to check only version and pathlevel (array index 0 - 1)
-            #
-            LINUX_VERSION="$(uname -r | tr '-' '\n' | head -n 1)"
-            LINUX_VERSION=($(echo $LINUX_VERSION | tr '.' '\n'))
-
-            # check if we can't increase max swap percent over 100
-            if [[ "5" -le "${LINUX_VERSION[0]}" ]]; then
-                if [[ "5" -ne ${LINUX_VERSION[0]} ]]; then
-                    # here only if is 6.x.y kernel
-                    echo "Your kernel is over 6.x.y"
-                    VALUE="200"
+AddSwap() {
+    if [ $MEMORY_RAM -lt 16 ] && [ $MEMORY_SWAP -lt 30 ]; then
+        echo "Your ram is less than 16 GB ($MEMORY_RAM GB) and your current swap is $MEMORY_SWAP GB"
+        read -p "Do you want to add a swap partition? [y/N]: " enable_swap
+        if [ "$enable_swap" == "y" ] || [ "$enable_swap" == "Y" ]; then
+            echo "1) Swap on disk partition"
+            echo "2) Swap on file"
+            echo "0 (or other) exit"
+            read -p "Chose [1-2]: " enable_swap
+            if [[ "$enable_swap" == "1" ]]; then
+                printf "${RED}WARNING: This script will FORMAT all the partition selected as SWAP."
+                printf "\nIf you are not sure what do selection default N${RST}\n"
+                read -p "Do you want continue? [y/N]" ImSure
+                if [ "$ImSure" == "y" ] || [ "$ImSure" == "Y" ]; then
+                    SwapOnPartition
                 else
-                    if [[ "10" -le ${LINUX_VERSION[1]} ]]; then
-                        # here if is over or equal 5.10.y kernel
-                        echo "Your kernel is over 5.10.y"
-                        VALUE="200"
-                    fi
+                    echo "Abort."
                 fi
-            else
-                echo "You have old kernel."
-            fi
-
-            if [[ $(cat $SWAPPINESS) -ne $VALUE ]]; then
-                echo "Increase the interaction with the swap from $(cat $SWAPPINESS) to $VALUE..."
-                sudo su -c "echo $VALUE > $SWAPPINESS"
-                echo " "
+            elif [[ "$enable_swap" == "2" ]]; then
+                SwapOnFile
             fi
         fi
     fi
-fi
+}
+
+#
+# Increase swap ratio if your machine is less than 16 GB of RAM
+#
+SwapRatio() {
+    if [[ $MEMORY_RAM -lt 16 ]]; then
+        if free | awk '/^Swap:/ {exit !$2}'; then
+            SWAPPINESS="/proc/sys/vm/swappiness"
+            if [ -f $SWAPPINESS ]; then
+                #
+                # 200 is max for linux 5.10 or over.
+                # 100 is max for linux 5.4 or before.
+                #
+                VALUE="100" # 100 by default
+                #
+                # put VERSION - PATCHLEVEL - SUBLEVEL in array
+                # but we need to check only version and pathlevel (array index 0 - 1)
+                #
+                LINUX_VERSION="$(uname -r | tr '-' '\n' | head -n 1)"
+                LINUX_VERSION=($(echo $LINUX_VERSION | tr '.' '\n'))
+
+                # check if we can't increase max swap percent over 100
+                if [[ "5" -le "${LINUX_VERSION[0]}" ]]; then
+                    if [[ "5" -ne ${LINUX_VERSION[0]} ]]; then
+                        # here only if is 6.x.y kernel
+                        echo "Your kernel is over 6.x.y"
+                        VALUE="200"
+                    else
+                        if [[ "10" -le ${LINUX_VERSION[1]} ]]; then
+                            # here if is over or equal 5.10.y kernel
+                            echo "Your kernel is over 5.10.y"
+                            VALUE="200"
+                        fi
+                    fi
+                else
+                    echo "You have old kernel."
+                fi
+
+                if [[ $(cat $SWAPPINESS) -ne $VALUE ]]; then
+                    echo "Increase the interaction with the swap from $(cat $SWAPPINESS) to $VALUE..."
+                    sudo su -c "echo $VALUE > $SWAPPINESS"
+                    echo " "
+                fi
+            fi
+        fi
+    fi
+}
+
+#
+# Get RAM size.
+# If RAM < 16 GB optimize swap usage.
+# Check also for already swap partition.
+#
+loadMemory
+
+#
+# git config
+#
+set_git_user
+
+#
+# Install akhilnarang Scripts
+#
+script_install
+
+#
+# Default configuration of ccache in bashrc
+#
+ccache_settings
+
+#
+# Chose if u want add swap with partition or with file.
+#
+AddSwap
+
+#
+# Increase swap usage from /proc/sys/vm/swappiness
+#
+SwapRatio
